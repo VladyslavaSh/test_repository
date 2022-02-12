fb1856b3455d0f02dc5daf9c044d7003a10984f8
Alyona <kalytenko.alyona@gmail.com>
2022-02-12 22:01:04 +0200

    Create hero for one player

--- src/main/java/ee/taltech/game/Hero.java
+++ src/main/java/ee/taltech/game/Hero.java
@@ -2,6 +2,7 @@ package ee.taltech.game;
 
 public class Hero {
     private String name;
+    private Fight fight;
 
     public Hero(String name) {
         this.name = name;
@@ -9,5 +10,17 @@ public class Hero {
 
     public String getName() {
         return name;
+    }
+
+    public void setFight(Fight fight) {
+        this.fight = fight;
+    }
+
+    public void doFight(){
+        if (fight == null){
+            return;
+        }
+
+        System.out.print("I was fighting");
     }
 }
